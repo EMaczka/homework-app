6a5c1dad0088e288c440af9a0134b61970e5b05e
Elzbieta Maczka <umbrela95@o2.pl>
2021-10-16 21:37:22 +0200

    First commit

---
+++ src/main/java/com/doyourhomework/homeworkapp/HomeworkApp.java
@@ -0,0 +1,13 @@
+package com.doyourhomework.homeworkapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/doyourhomework/homeworkapp/HomeworkAppTests.java
@@ -0,0 +1,13 @@
+package com.doyourhomework.homeworkapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
